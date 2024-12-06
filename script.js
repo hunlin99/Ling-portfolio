e8308aefa2343a9197dbcf03abb02143ae213f17
Ling Chang <127913579+hunlin99@users.noreply.github.com>
2024-12-06 07:40:30 +0000

    finish nav bar interactive setting and scrolling content on landing page

--- script.js
+++ script.js
@@ -10,3 +10,20 @@ navLinks.forEach(link => {
         link.classList.add("active"); 
     }
 });
+
+//cover content scroll
+function vscroll() {
+    var frtEle = $('.scroll-content li:first');
+    frtEle.animate({ 'margin-top': -$('.scroll-content li').height() }, 500, function () {
+        frtEle.css('margin-top', 0);
+        $('.scroll-content').append(frtEle);
+    })
+}
+var startInterval = setInterval(vscroll, 3000);
+
+// hover to stop
+$('.scroll-content li').hover(function () {
+    clearTimeout(startInterval);
+}, function () {
+    startInterval = setInterval(vscroll, 3000);
+});
