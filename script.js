8e5c5d85208b4d7c1b23d0f1415a1fd0175ae3af
Ling Chang <127913579+hunlin99@users.noreply.github.com>
2024-12-09 18:03:18 +0000

    add loading animation

--- script.js
+++ script.js
@@ -1,24 +1,5 @@
-// Get the current page
-const currentPage = window.location.pathname.split("/").pop();
-
-// Select all the <a>
-const navLinks = document.querySelectorAll("nav a");
-
-
-navLinks.forEach(link => {
-    if (link.getAttribute("href") === currentPage) {
-        link.classList.add("active"); 
-    }
-});
 
 //cover content scroll
-function vscroll() {
-    var frtEle = $('.scroll-content li:first');
-    frtEle.animate({ 'margin-top': -$('.scroll-content li').height() }, 500, function () {
-        frtEle.css('margin-top', 0);
-        $('.scroll-content').append(frtEle);
-    })
-}
 var startInterval = setInterval(vscroll, 3000);
 
 // hover to stop
@@ -26,4 +7,37 @@ $('.scroll-content li').hover(function () {
     clearTimeout(startInterval);
 }, function () {
     startInterval = setInterval(vscroll, 3000);
+});
+
+// Scroll content animation
+function vscroll() {
+    var frtEle = $('.scroll-content li:first');
+    frtEle.animate({ 'margin-top': -$('.scroll-content li').height() }, 500, function () {
+      frtEle.css('margin-top', 0);
+      $('.scroll-content').append(frtEle);
+    });
+  }
+  var startInterval = setInterval(vscroll, 3000);
+
+  // Hover to stop scrolling
+  $('.scroll-content li').hover(function () {
+    clearTimeout(startInterval);
+  }, function () {
+    startInterval = setInterval(vscroll, 3000);
+  });
+
+
+  // Select the loader container and video element
+const loader = document.querySelector('.loader-container');
+const video = document.getElementById('video');
+
+// Wait for the video to be ready to play (canplaythrough event)
+video.addEventListener('canplaythrough', function () {
+    // Add a class to hide the loader with a smooth transition
+    loader.classList.add('hide-loader');
+
+    // Remove the loader after the fade-out transition is complete
+    loader.addEventListener('transitionend', function () {
+        loader.remove(); // Remove the loader element from the DOM
+    });
 });
